fc52c4d74555ba7b2847df96b1e7a5345f408bbd
iyeonghun <twosoull@naver.com>
2025-01-06 21:24:54 +0900

    first commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:message="getTodoList"></TodoInput>
+    <TodoList props="todoList"></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
+
+export default {
+  methods: {
+    getTodoList() {
+      this.todoList = localStorage.getItem;
+
+    }
+  },
+  data(){
+    return{
+      todoList : ''
+    }
+  },
+  components: {
+    'TodoHeader' : TodoHeader,
+    'TodoList' : TodoList,
+    'TodoInput' : TodoInput,
+    'TodoFooter' : TodoFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button @click="addTodo"> add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods : {
+            addTodo() {
+                console.log(this.newTodoItem);
+                localStorage.setItem(this.newTodoItem,this.newTodoItem);
+                this.newTodoItem = '';
+
+                this.$emit("message");
+            }
+        }, 
+        data() {
+            return {
+                newTodoItem : ''
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
